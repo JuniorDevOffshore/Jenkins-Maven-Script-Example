c4dc83719bed2a0024f587403e488557286fe582
Anandh-Raja <anandhraja1030@gmail.com>
2024-08-09 12:33:36 +0530

    Sonar Build

---
+++ SonarBuildJenkinsScript.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                // Clean and compile the Maven project
+                sh 'mvn clean compile'
+            }
+        }
+        stage('Test') {
+            steps {
+                // Run tests
+                sh 'mvn test'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                // Package and deploy the Maven project (this might vary depending on your deployment strategy)
+                sh 'mvn package'
+                // Optionally, add a deploy command
+                // sh 'mvn deploy' (uncomment if you have a deploy stage in Maven)
+            }
+        }
+    }
+}
